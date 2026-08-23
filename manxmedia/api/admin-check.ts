c006ff0a3a5f489ce6404418e815a52d7c007352
crowntechrblx-maker <crowntechrblx@gmail.com>
2026-08-23 18:27:39 +0000

    Remove stray nested manxmedia/ folder from previous upload

--- manxmedia/api/admin-check.ts
+++
@@ -1,7 +0,0 @@
-import { isAdminRequest } from '../lib/auth';
-
-export default async function handler(req: any, res: any) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.status(200).json({ isAdmin: isAdminRequest(req.headers.cookie) });
-}
